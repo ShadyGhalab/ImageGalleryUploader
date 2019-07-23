93c6d939fd4146fbefea217f9a445e7a71658b44
Shady Mustafa <>
2019-07-23 12:46:23 +0200

    Remove the main storyboard and add navigation controller

--- ImageUploader/Controllers/ImageGalleryNavigationController.swift
+++ ImageUploader/Controllers/ImageGalleryNavigationController.swift
@@ -8,14 +8,7 @@
 
 import UIKit
 
-class ImageGalleryNavigationController: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+class ImageGalleryNavigationController: UINavigationController, StoryboardMakeable {
+    static var storyboardName: String = "ImageGallery"
+    typealias StoryboardMakeableType = ImageGalleryNavigationController
 }
